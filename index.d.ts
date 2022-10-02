6b65eb255fe2beaccff383fc06743320e043e3a2
da7a90-backup <sidbarrack@gmail.com>
2022-10-02 15:59:30 +0000

    Fix: replaced cluster with datasource in module definition

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ export function mongodb(params: {
 	apiId?: string;
 	apiRegion?: string;
 	apiUrl?: string;
-	cluster?: string;
+	dataSource?: string;
 	database?: string;
 	collection?: string;
 	fetch?: (url: string, options: { method: 'POST', headers: { [key: string]: string }, body: string }) => Promise<{ status: number, headers: { [key: string]: string }, json: () => Promise<unknown>, text: () => Promise<string> }>;
